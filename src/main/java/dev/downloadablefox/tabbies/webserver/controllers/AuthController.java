b4ef18fc40299f974c86e5e37ea72f81b84670f0
gyusba1 <felipe.garridoaf@gmail.com>
2025-03-04 09:55:20 -0500

    ola, hice el login muak

--- src/main/java/dev/downloadablefox/tabbies/webserver/controllers/AuthController.java
+++ src/main/java/dev/downloadablefox/tabbies/webserver/controllers/AuthController.java
@@ -1,8 +1,14 @@
 package dev.downloadablefox.tabbies.webserver.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/auth")
 public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
     
 }
