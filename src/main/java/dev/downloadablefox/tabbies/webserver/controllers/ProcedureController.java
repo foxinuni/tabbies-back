237e9ab2deea9b4e8299e8b849fa21e15819f712
DownloadableFox <me@downloadablefox.dev>
2025-05-05 22:48:09 -0500

    fixed several warnings

--- src/main/java/dev/downloadablefox/tabbies/webserver/controllers/ProcedureController.java
+++ src/main/java/dev/downloadablefox/tabbies/webserver/controllers/ProcedureController.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- src/main/java/dev/downloadablefox/tabbies/webserver/services/procedure/ProcedureServiceImpl.java
+++ src/main/java/dev/downloadablefox/tabbies/webserver/services/procedure/ProcedureServiceImpl.java
@@ -1,7 +1,6 @@
 package dev.downloadablefox.tabbies.webserver.services.procedure;
 
 import java.util.Collection;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
